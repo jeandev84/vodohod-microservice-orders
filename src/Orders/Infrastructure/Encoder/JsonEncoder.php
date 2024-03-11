ec5bfbb259a614a51894b34cbaf32975ec5878d2
Jean Claude <jeanyao@ymail.com>
2024-03-11 19:14:09 +0500

    Implementation Method validation Process

--- src/Orders/Infrastructure/Encoder/JsonEncoder.php
+++ src/Orders/Infrastructure/Encoder/JsonEncoder.php
@@ -3,7 +3,8 @@ declare(strict_types=1);
 
 namespace App\Orders\Infrastructure\Encoder;
 
-use Encoder\JsonEncoderInterface;
+
+use App\Orders\Application\Contract\Encoder\JsonEncoderInterface;
 
 /**
  * JsonEncoder
